4d7ce04bfb766335662c571cf56afded387ecb16
Luis Briceño <siulbriceno20@gmail.com>
2025-02-12 17:52:56 -0400

    feat: Add loading and error handling to AppUI component

--- src/App/AppUi.js
+++ src/App/AppUi.js
@@ -3,6 +3,10 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+
 
 function AppUI({loading, error, totalTodos, completedTodos, searchValue, setSearchValue, searchedTodos, completeTodo, deleteTodo }) {
     return (
@@ -15,9 +19,9 @@ function AppUI({loading, error, totalTodos, completedTodos, searchValue, setSear
             />
 
             <TodoList>
-                { loading && <p>loading...</p> }
-                { error && <p>error...</p> }
-                { (!loading && !searchedTodos.length) && <p>There are no todos</p> }
+                { loading && <> <TodosLoading /><TodosLoading /><TodosLoading /> </>}
+                { error && <TodosError error={error} /> }
+                { (!loading && !searchedTodos.length) && <EmptyTodos /> }
 
                 {searchedTodos.map(todo => (
                     <TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => completeTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} />
---
+++ src/TodosError/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TodosError.css';
+
+function TodosError() {
+
+  return (
+    <p>Cargando...</p>
+  );
+}
+
+export { TodosError };
